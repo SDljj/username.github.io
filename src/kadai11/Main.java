a32b702f2d3dbfba4b3a36f17224fabf219cc607
SDljj <g15958yc@gm.tsuda.ac.jp>
2018-04-12 13:40:41 +0900

    Initial commit

---
+++ src/kadai11/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package kadai11;
+
+/**
+ * @author g15958yc
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Eclipseを使うのは初めてですが、なんとなく使いやすいなと感じているので、使いこなせるようになりたいです");
+		// TODO Auto-generated method stub
+
+	}
+
+}
